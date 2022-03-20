54e057aa9fcbac2dbe48bc0403e86e340204e08f
Chris <cochsenreither@protonmail.com>
2022-03-19 17:31:09 -0700

    array solutions

---
+++ Data Structures/Javascript/ArrayManipulation.js
@@ -0,0 +1,75 @@
+// https://www.hackerrank.com/challenges/crush/problem
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'arrayManipulation' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function arrayManipulation(n, queries) {
+    // Write your code here
+    const arr = Array.from(Array(n+1), () => 0);
+    let x = 0;
+    let max = 0;
+
+    for (let i = 0; i < queries.length; i++) {
+        const [start, end, val] = queries[i];
+        arr[start] += val;
+        if (end + 1 <= n) arr[end + 1] -= val;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        x = x + arr[i];
+        if (max < x) max = x;
+    }
+
+    return max;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(m);
+
+    for (let i = 0; i < m; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = arrayManipulation(n, queries);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
