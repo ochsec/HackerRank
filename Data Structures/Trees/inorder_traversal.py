46e947114694670ca301bcd2ae4e6f55161cb672
Chris <cochsenreither@protonmail.com>
2022-03-22 15:18:12 -0700

    tree traversal

---
+++ Data Structures/Trees/inorder_traversal.py
@@ -0,0 +1,72 @@
+# https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def inOrder(root, level=1, ls=[]):
+    #Write your code here
+    ls.append(root.info)
+    root.level = level
+    
+    if root.left:
+        ls, level = inOrder(root.left, level+1, ls)
+        
+    if root.right:
+        ls, level = inOrder(root.right, level+1, ls)
+    
+    if root.level == 1:
+        for num in sorted(ls):
+            print(num, end=" ")
+    else:
+        return sorted(ls), level
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+inOrder(tree.root)
