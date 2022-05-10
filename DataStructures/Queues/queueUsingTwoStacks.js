10737ab7bbe3362e91b7dd078affbc012044032d
Chris <cochsenreither@protonmail.com>
2022-05-10 11:35:58 -0700

    using list/array as queue

--- DataStructures/Queues/queueUsingTwoStacks.js
+++ DataStructures/Queues/queueUsingTwoStacks.js
@@ -4,34 +4,17 @@ function processData(input) {
         .filter((x, index) => index != 0)
         .map(y => y.split(' ')
             .map(z => parseInt(z)))
-    let A = [] // Queue stack
-    let B = [] // Dequeue stack
-    output = [] // The numbers to return to be printed
+    let Q = [] // Queue
 
     ops.forEach(op => {
         if (op[0] == 1) {
             // Enqueue
-            if (A.length == 0) {
-                while (B.length > 0) {
-                    A.push(B.pop())
-                }
-            }
-            A.push(op[1])
+            Q.push(op[1])
         } else if (op[0] == 2) {
             // Dequeue
-            if (B.length == 0) {
-                while (A.length > 0) {
-                    B.push(A.pop())
-                }
-            }
-            B.pop()
+            Q.shift()
         } else {
-            if (B.length == 0) {
-                while (A.length > 0) {
-                    B.push(A.pop())
-                }
-            }
-            console.log(B[B.length - 1])            
+            console.log(Q[0])            
         }
     })
 } 
--- DataStructures/Queues/queue_using_two_stacks.py
+++ DataStructures/Queues/queue_using_two_stacks.py
@@ -1,82 +1,30 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import os
 
-# class Stack:
-#     def __init__(self):
-#         self.count = 0
-#         self.items = {}
-
-#     def __str__(self):
-#         if self.is_empty():
-#             return ''
-#         s = str(self.items[0])
-#         for i in range(1, self.count):
-#             s = s + ',' + str(self.items[i])
-#         return s
-
-#     def push(self, element):
-#         self.items[self.count] = element
-#         self.count = self.count + 1
-
-#     def pop(self):
-#         if self.is_empty():
-#             return None
-#         self.count = self.count - 1
-#         last = self.items[self.count]
-#         del self.items[self.count]
-#         return last
-
-#     def peek(self):
-#         if self.is_empty():
-#             return None
-#         return self.items[self.count - 1]
-
-#     def size(self):
-#         return self.count
-
-#     def is_empty(self):
-#         return self.count == 0
-
-#     def clear(self):
-#         self.items = {}
-#         self.count = 0
-
 def process(queries):
-    A = [] # Queue stack
-    B = [] # Dequeue stack
+    Q = [] # Queue stack
     output = [] # The numbers to return to be printed
         
     for q in queries:
         if q[0] == 1:
             # Enqueue
-            if len(A) == 0:
-                while len(B) != 0:
-                    A.append(B.pop())
-            A.append(q[1])
+            Q.append(q[1])
         elif q[0] == 2:
             # Dequeue
-            if len(B) == 0:
-                while len(A) != 0:
-                    B.append(A.pop())
-            B.pop()
+            Q.pop(0)
         else:
-            if len(B) == 0:
-                while len(A) != 0:
-                    B.append(A.pop())               
-            output.append(B[len(B) - 1])
+            # Add to output
+            output.append(Q[0])
        
     return output
-                
                 
 if __name__ == '__main__':
     lines = []
     queries = []
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
     n = int(input().strip())
-
     for i in range(n):
         lines.append(input())
-
     for l in lines:
         args = l.split(' ')
         args = list(map(int, args))
@@ -85,6 +33,5 @@ if __name__ == '__main__':
     output = process(queries)
     
     for num in output:
-        print(num)
-    #     fptr.write(f"{num}\n")
-    # fptr.close()
+        fptr.write(f"{num}\n")
+    fptr.close()
